5520da57011486f094cc0af4d10263fc7a38761a
Ethan <etarrer@gmail.com>
2024-02-08 16:20:06 -0800

    Initial Commit

---
+++ tennisscorekeeper Watch App/tennisscorekeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  tennisscorekeeperApp.swift
+//  tennisscorekeeper Watch App
+//
+//  Created by Ethan Tarrer on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct tennisscorekeeper_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tennisscorekeeper/tennisscorekeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  tennisscorekeeperApp.swift
+//  tennisscorekeeper
+//
+//  Created by Ethan Tarrer on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct tennisscorekeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
